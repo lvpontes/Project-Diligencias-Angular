0df3726396a6c77a1b99e5fa9608da63de5a88e4
lvpontes <levpontes@gmail.com>
2017-05-21 20:51:01 -0300

    Delete edit.js

--- edit.js
+++
@@ -1,8 +0,0 @@
-var classApp = angular.module('classApp', []);
-
-classApp.controller('classCtrl', function ($scope) {
-	$scope.isActive = false;
-  $scope.activeButton = function() {
-    $scope.isActive = !$scope.isActive;
-  }  
-});
