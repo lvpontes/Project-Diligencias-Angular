c292fcfa58c536e057fa437474adc77bb7c9fb28
lvpontes <levpontes@gmail.com>
2017-05-21 23:41:30 -0300

    Add files via upload

--- js/edit.js
+++ js/edit.js
@@ -1,8 +1,25 @@
-var classApp = angular.module('classApp', []);
+var diligencias = angular.module('diligencias', []);
 
-classApp.controller('classCtrl', function ($scope) {
-	$scope.isActive = false;
+diligencias.controller('orders', function ($scope, $http) {
+  $scope.is_active = false;
   $scope.activeButton = function() {
-    $scope.isActive = !$scope.isActive;
-  }  
+    $scope.is_active = !$scope.is_active;
+  } 
+  $scope.order_by = 'diligencia_type';
+  $scope.orderBy = function(type){
+    $scope.order_by = type;
+  };
+  
+  $http({
+    method: 'GET',
+    url: 'https://api.diligeiro.com.br/v2/diligencias/public'
+  }).then(function successCallback(response) {
+    $scope.diligencias = response.data.results;
+    for(var i = 0; i <= $scope.diligencias.length; i++ ){
+      $scope.diligencias[i].city = $scope.diligencias[i].city.replace(", " , " - "); 
+    };
+  }, function errorCallback(response) {
+    console.log(response);
+  });
 });
+
